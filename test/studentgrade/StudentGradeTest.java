d8f96d7f9be4ee2e964e22b0580b6fdbfce2602e
norahsull <norahsultan2003@gmail.com>
2025-03-20 01:14:50 +0300

    Upgraded StudentGradeTest to JUnit 5.

--- test/studentgrade/StudentGradeTest.java
+++ test/studentgrade/StudentGradeTest.java
@@ -1,23 +1,55 @@
+//package studentgrade;
+//
+//import junit.framework.TestCase;
+//
+///* JUnit 3.8.1 */
+//public class StudentGradeTest extends TestCase {
+//    
+//    public StudentGradeTest(String testName) {
+//        super(testName);
+//    }
+//
+//    public void testGetGrade95() {
+//        System.out.println("getGrade 95");
+//        int mark = 95;
+//        String expResult = "A";
+//        String result = StudentGrade.getGrade(mark);
+//        assertEquals(expResult, result);
+//    }
+//
+//    public void testGetGrade85() {
+//        System.out.println("getGrade 85");
+//        int mark = 85;
+//        String expResult = "B";
+//        String result = StudentGrade.getGrade(mark);
+//        assertEquals(expResult, result);
+//    }
+//    
+//    public void testGetGrade75() {
+//        System.out.println("getGrade 85");
+//        int mark = 75;
+//        String expResult = "F";
+//        String result = StudentGrade.getGrade(mark);
+//        assertEquals(expResult, result);
+//    }
+//}
 package studentgrade;
 
-import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/* JUnit 3.8.1 */
-public class StudentGradeTest extends TestCase {
-    
-    public StudentGradeTest(String testName) {
-        super(testName);
-    }
-
-    public void testGetGrade95() {
+public class StudentGradeTest {
+    @Test
+    void testGetGrade95() {
         System.out.println("getGrade 95");
         int mark = 95;
         String expResult = "A";
         String result = StudentGrade.getGrade(mark);
         assertEquals(expResult, result);
     }
-
-    public void testGetGrade85() {
+    
+    @Test
+    void testGetGrade85() {
         System.out.println("getGrade 85");
         int mark = 85;
         String expResult = "B";
@@ -25,8 +57,9 @@ public class StudentGradeTest extends TestCase {
         assertEquals(expResult, result);
     }
     
-    public void testGetGrade75() {
-        System.out.println("getGrade 85");
+    @Test
+    void testGetGrade75() {
+        System.out.println("getGrade 75"); // Fixed typo from "85" to "75"
         int mark = 75;
         String expResult = "F";
         String result = StudentGrade.getGrade(mark);
